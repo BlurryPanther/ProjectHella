9180a79fcb8865cc37cc03c1164076a65d55a270
Burmuruk <santoyo.herrera.david@gmail.com>
2024-01-24 00:36:14 -0600

    Clases parciales y base.

---
+++ Assets/E_Scripts/Mechanics/TriggerDialog.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TriggerDialog : Interactable
+{
+    public bool ho;
+    public override void Action()
+    {
+        base.Action();
+
+        //Show dialog
+    }
+}
+
+public class Door : Interactable
+{
+    public bool hi;
+    public override void Action()
+    {
+        base.Action();
+
+        //Show dialog
+    }
+}
+
+public class Item : Interactable
+{
+    public override void Action()
+    {
+        base.Action();
+
+        //Show dialog
+    }
+}
+
+public class CheckPoint : Interactable
+{
+    public override void Action()
+    {
+        base.Action();
+
+        //Show dialog
+    }
+}
